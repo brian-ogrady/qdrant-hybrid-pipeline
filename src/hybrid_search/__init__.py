520c2e31973a8425e68f97c2541bcba7c4bcbcc1
elefante37 <brianogrady@proton.me>
2025-04-21 11:56:54 -0400

    Adding sentence-transformers

--- src/hybrid_search/__init__.py
+++ src/hybrid_search/__init__.py
@@ -6,9 +6,10 @@ that leverage multiple embedding types for improved search performance.
 """
 
 from .hybrid_pipeline import HybridPipeline
-from .hybrid_pipeline_config import HybridPipelineConfig
+from .hybrid_pipeline_config import HybridPipelineConfig, SentenceTransformerEmbedding
 
 __all__ = [
     "HybridPipeline",
     "HybridPipelineConfig",
+    "SentenceTransformerEmbedding",
 ]
